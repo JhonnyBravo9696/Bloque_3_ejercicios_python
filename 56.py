971b661621c027f9f2a9248d625e06095645700e
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-21 09:00:50 +0200

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,41 @@
+numero = int(input("introduce un numero de mes (1-12): "))
+
+dias = [31,28,31,30,31,30,31,30,30,31,30,31]
+
+nombre_mes = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+
+if numero == 1:
+  print (f'{nombre_mes[0]} tiene {dias[0]} dias')
+
+if numero == 2:
+  print (f'{nombre_mes[1]} tiene {dias[1]} dias')
+
+if numero == 3:
+  print (f'{nombre_mes[2]} tiene {dias[2]} dias')
+
+if numero == 4:
+  print (f'{nombre_mes[3]} tiene {dias[3]} dias')
+
+if numero == 5:
+  print (f'{nombre_mes[4]} tiene {dias[4]} dias')
+
+if numero == 6:
+  print (f'{nombre_mes[5]} tiene {dias[5]} dias')
+
+if numero == 7:
+  print (f'{nombre_mes[6]} tiene {dias[6]} dias')
+
+if numero == 8:
+  print (f'{nombre_mes[7]} tiene {dias[7]} dias')
+
+if numero == 9:
+  print (f'{nombre_mes[8]} tiene {dias[8]} dias')
+
+if numero == 10:
+  print (f'{nombre_mes[9]} tiene {dias[9]} dias')
+
+if numero == 11:
+  print (f'{nombre_mes[10]} tiene {dias[10]} dias')
+
+if numero == 12:
+  print (f'{nombre_mes[11]} tiene {dias[11]} dias')
