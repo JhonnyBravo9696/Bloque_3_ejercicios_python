ccffbea0503503cd8299d7a6c066f59bc2c4e8fb
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-21 22:21:48 +0200

    Create 42.py

---
+++ 42.py
@@ -0,0 +1,8 @@
+cadena = input("introduce: ")
+subcadena = input("introduce la subcadena: ")
+
+if cadena[0] == subcadena[0]:
+  print (f'ambas cadenas empiezan por {cadena[0]}')
+else:
+  print (f'las cadenas no empiezan igual')
+
