7a7fdbb4c84777bcbd470617881aff2f6ebdddd0
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-21 22:23:39 +0200

    Create 45.py

---
+++ 45.py
@@ -0,0 +1,8 @@
+nombre = input ("introduce tu nombre: ")
+apellido1 = input ("introduce tu primer apellido: ")
+apellido2 = input ("introduce tu segundo apellido: ")
+
+iniciales = nombre[0].upper() + apellido1[0].upper() + apellido2[0].upper()
+
+print(f' iniciales: {iniciales}')
+
