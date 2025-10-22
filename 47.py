be4c03d3cae9d6875e7475cebad41a9f9b86c7a5
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-22 09:03:51 +0200

    Create 47.py

---
+++ 47.py
@@ -0,0 +1,6 @@
+cadena = input("introduce texto: ")
+caracter = input("introduce el primer caracter: ")
+caracter2 = input("introduce el segunddo caracter: ")
+
+cadenafinal = cadena.replace(caracter,caracter2)
+print (f'cadena de texto final {cadenafinal}')
