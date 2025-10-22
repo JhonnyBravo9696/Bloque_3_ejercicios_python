8bf0f475eaa125fa0a7a588bcddf36e698290022
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-22 10:31:46 +0200

    Create 46.py

---
+++ 46.py
@@ -0,0 +1,19 @@
+cadena = input("introduce tu texto de 5 a 10 caracteres :")
+
+if len(cadena) == 5:
+  print (cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
+
+if len(cadena) == 6:
+  print (cadena[5],cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
+
+if len(cadena) == 7:
+  print (cadena[6],cadena[5],cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
+
+if len(cadena) == 8:
+  print (cadena[7],cadena[6],cadena[5],cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
+
+if len(cadena) == 9:
+  print (cadena[8],cadena[7],cadena[6],cadena[5],cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
+
+if len(cadena) == 10:
+  print (cadena[9],cadena[8],cadena[7],cadena[6],cadena[5],cadena[4],cadena[3],cadena[2],cadena[1],cadena[0])
